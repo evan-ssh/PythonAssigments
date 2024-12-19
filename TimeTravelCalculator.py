47cd124f2b7acf907200c99e2c0dbb6db673672d
evan-ssh <62630880+evan-ssh@users.noreply.github.com>
2024-12-19 19:45:56 -0330

    Done

---
+++ TimeTravelCalculator.py
@@ -0,0 +1,11 @@
+def main():
+    miles_traveled = int(input(""))
+    miles_per_hours = int(input(""))
+    time_in_hours = miles_traveled / miles_per_hours
+    time_in_minutes = miles_traveled % miles_per_hours
+
+    print("Estimated Travel Time")
+    print("Hours:", time_in_hours)
+    print("Minutes:", time_in_minutes)
+main()
+
